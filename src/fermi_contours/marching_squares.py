29ad4516c78a3b4192d4a2146d03fc418d25da07
Pablo Piskunow <pablo.perez.piskunow@gmail.com>
2022-11-22 21:18:15 +0100

    more strict type checking

--- src/fermi_contours/marching_squares.py
+++ src/fermi_contours/marching_squares.py
@@ -14,10 +14,21 @@ and extended to take into account periodic boundaries and other edge-cases.
 
 from warnings import warn
 import numpy as np
+import numpy.typing as npt
+from typing import Any, Callable, Set, Sequence
+
+PairInt = tuple[int, int]
+PairFloat = tuple[float, float]
+LPInt = list[PairInt]
+LPFloat = list[PairFloat]
 
 
-class MarchingSquares():
-    """Marching square class
+def _dummy(x: float, y: float) -> float:
+    return x + y
+
+
+class MarchingSquares:
+    """Marching square class.
 
     Parameters
     ----------
@@ -49,45 +60,58 @@ class MarchingSquares():
         then `x_max` should be mapped to `x_min`, and `y_max` to `y_min`.
     """
 
-    def __init__(self, grid_values=None, func=None, bounds=None, res=None,
-                 open_contours=True, periodic=False):
+    def __init__(
+        self,
+        grid_values: npt.NDArray[np.float_] | None = None,
+        func: Callable[[float, float], float] | None = None,
+        bounds: tuple[PairInt, PairInt] | tuple[PairFloat, PairFloat] | None = None,
+        res: int | PairInt | None = None,
+        open_contours: bool = True,
+        periodic: bool = False,
+    ) -> None:
 
         # compute the coordinates grid
+        if bounds is None and grid_values is None:
+            raise ValueError("Either 'bounds' or 'grid_values' must be provided.")
         if bounds is None:
-            try:
-                assert grid_values is not None
-            except AssertionError:
-                raise ValueError("Either 'bounds' or 'grid_values' must be "
-                                 "provided.")
             n, m = np.array(grid_values).shape
             bounds = ((0, n), (0, m))
             # discard `res`
             if res is not None:
-                raise ValueError("'res' is discarded when 'bounds' is not "
-                                 "provided.")
+                raise ValueError("'res' is discarded when 'bounds' is not provided.")
             res = (n, m)
         self.bounds = bounds
-        if isinstance(res, int):
-            res = (res, res)
-        self.res = res
+        if res is None:
+            raise ValueError("'res' is discarded when 'bounds' is not provided.")
+        elif isinstance(res, int):
+            _res = (res, res)
+        elif len(res) == 2:
+            _res = res
+        else:
+            raise ValueError("'res' must be 'None', an 'int', or a pair of ints.")
+        self.res = _res
 
         # the coordinates are computed with a property, it needs to know
         # about the periodic condition (to include the endpoint or not)
         self.periodic = periodic
 
         # compute the grid values
-        self.func = func
+        if callable(func):
+            self.func = func
+        else:
+            if grid_values is None:
+                raise ValueError(
+                    "'func' must be a callable when 'grid_values' is 'None'."
+                )
+            self.func = _dummy
         if grid_values is None:
-            if not callable(self.func):
-                raise ValueError("'func' must be a callable when "
-                                 "'grid_values' is 'None'.")
             self.grid_values = self._compute_grid_values()
         else:
             self.grid_values = np.atleast_2d(grid_values)
 
         self.open_contours = open_contours
 
-    def __call__(self, level=0):
+    def __call__(self, level: int = 0) -> list[LPFloat]:
         """Calcualte the Fermi contours for a Fermi level.
 
         Sets values for the attributes defined below.
@@ -104,8 +128,8 @@ class MarchingSquares():
         """
         contours_cells, contour_paths = self._find_contours(level)
         # check for repeated cells and return only the largest
-        digested_contour_cells = []
-        digested_contour_paths = []
+        digested_contour_cells: list[Any] = []
+        digested_contour_paths: list[Any] = []
         for c_cells, v_cells in zip(contours_cells, contour_paths):
             # check that this does not belong to digest
             updated = False
@@ -124,25 +148,27 @@ class MarchingSquares():
         return [list(d.values()) for d in digested_contour_paths]
 
     @property
-    def grid_points(self):
+    def grid_points(self) -> tuple[npt.NDArray[np.float_], npt.NDArray[np.float_]]:
         x1, x2 = self.bounds[0]
         y1, y2 = self.bounds[1]
         n_x, n_y = self.res
         endpoint = not self.periodic
-        x_array = np.linspace(x1, x2, n_x, endpoint=endpoint)
-        y_array = np.linspace(y1, y2, n_y, endpoint=endpoint)
+        x_array = np.linspace(x1, x2, n_x, endpoint=endpoint, dtype=np.float_)
+        y_array = np.linspace(y1, y2, n_y, endpoint=endpoint, dtype=np.float_)
         return x_array, y_array
 
-    def _compute_grid_values(self):
+    def _compute_grid_values(self) -> npt.NDArray[np.float_]:
         x_array, y_array = self.grid_points
-        grid_values = np.ndarray(self.res, dtype=float)
+        grid_values: npt.NDArray[np.float_] = np.ndarray(self.res, dtype=np.float_)
 
         for ix in range(self.res[0]):
             for iy in range(self.res[1]):
                 grid_values[ix, iy] = self.func(x_array[ix], y_array[iy])
         return grid_values
 
-    def _find_contours(self, level):
+    def _find_contours(
+        self, level: float
+    ) -> tuple[list[LPInt], list[dict[PairInt, PairFloat]]]:
         """Get the coarse grid coordinates of one contour, or set of contours
 
         This is a fast version of the MarchinSquares algorithm, that uses
@@ -161,26 +187,33 @@ class MarchingSquares():
         # construct binary grid of 0's and 1's regions
         gridvals = self.grid_values < level
         cells = (
-            (gridvals[:-1, :-1] << 0) + (gridvals[1:, :-1] << 1)
-            + (gridvals[1:, 1:] << 2) + (gridvals[:-1, 1:] << 3))
+            (gridvals[:-1, :-1] << 0)
+            + (gridvals[1:, :-1] << 1)
+            + (gridvals[1:, 1:] << 2)
+            + (gridvals[:-1, 1:] << 3)
+        )
 
         grid_binary = 1 * gridvals
         # save the values that change after a x-shift
-        roll_x = np.logical_xor(grid_binary,
-                                np.roll(grid_binary, shift=-1, axis=0))
+        roll_x = np.logical_xor(grid_binary, np.roll(grid_binary, shift=-1, axis=0))
         # save the values that change after a y-shift
-        roll_y = np.logical_xor(grid_binary,
-                                np.roll(grid_binary, shift=-1, axis=1))
+        roll_y = np.logical_xor(grid_binary, np.roll(grid_binary, shift=-1, axis=1))
         # find the union of x-shifts and y-shifts
         roll_xy = np.logical_or(roll_x, roll_y)
         # the indices of the region contours are the `xys`
-        xys = {tuple(xy) for xy in np.argwhere(roll_xy)
-               if np.all(xy < (n_x - 1, n_y - 1))}  # filter last indices
+        xys = {
+            (x, y)
+            for (x, y) in np.argwhere(roll_xy)
+            if np.all((x, y) < (n_x - 1, n_y - 1))
+        }  # filter last indices
 
         contours_cells = []
         contour_paths = []
 
-        d_ij = None  # no direction yet, but needed for marching step reference
+        d_ij: PairInt = (
+            0,
+            0,
+        )  # no direction yet, but needed for marching step reference
 
         # we go through all the nontrivial grid points, but not visit the same
         # loop twice.
@@ -189,13 +222,15 @@ class MarchingSquares():
         if self.periodic:
             mod = (self.res[0] - 1, self.res[1] - 1)
             origin = (self.bounds[0][0], self.bounds[1][0])
-            periods = (self.bounds[0][1] - self.bounds[0][0],
-                       self.bounds[1][1] - self.bounds[1][0])
+            periods = (
+                self.bounds[0][1] - self.bounds[0][0],
+                self.bounds[1][1] - self.bounds[1][0],
+            )
 
         while xys:
             initial_point = min(xys)  # lexicographical minimum of tuples
-            single_contour = []
-            single_path = dict()
+            single_contour: LPInt = []
+            single_path: dict[PairInt, PairFloat] = dict()
             xys.discard(initial_point)
             ij = initial_point
 
@@ -204,23 +239,28 @@ class MarchingSquares():
             while True:
                 # make sure we went through all the indices in the contour
                 i, j = ij
-                middle_k = ((x_array[i] + x_array[i+1]) / 2,
-                            (y_array[j] + y_array[j+1]) / 2)
+                middle_k = (
+                    (x_array[i] + x_array[i + 1]) / 2,
+                    (y_array[j] + y_array[j + 1]) / 2,
+                )
 
                 try:
                     d_ij = marching_step(cells[ij], self.func, middle_k, d_ij)
                 except RuntimeError:
-                    warn('Saddle point not resolved.')
+                    warn("Saddle point not resolved.")
                     break
                 except TypeError as e:
                     if self.func is None:
-                        warn("Saddle point not resolved because 'func' is not provided.")
+                        warn(
+                            "Saddle point not resolved because 'func' is not provided."
+                        )
                         break
                     else:
                         raise TypeError(e)
 
-                xy = marching_cell_values(ij, d_ij, self.grid_values,
-                                          x_array, y_array, level, mod=mod)
+                xy = marching_cell_values(
+                    ij, d_ij, self.grid_values, x_array, y_array, level, mod=mod
+                )
                 i, j = np.array(ij, dtype=int) + d_ij
                 if mod is not None:
                     i = (i + mod[0]) % mod[0]
@@ -228,9 +268,9 @@ class MarchingSquares():
                     _x, _y = xy
                     _x = origin[0] + (_x - origin[0] + periods[0]) % periods[0]
                     _y = origin[1] + (_y - origin[1] + periods[1]) % periods[1]
-                    xy = tuple([_x, _y])
+                    xy = (_x, _y)
 
-                ij = tuple([i, j])
+                ij = (i, j)
 
                 try:
                     xys.remove(ij)
@@ -247,15 +287,17 @@ class MarchingSquares():
                     _ = cells[ij]
                     single_contour.append(ij)
                     single_path[ij] = xy
-                except (IndexError, AssertionError):
+                except (IndexError, AssertionError) as err:
                     contours_cells.append(single_contour)
                     contour_paths.append(single_path)
                     if self.open_contours:
                         break
                     else:
-                        raise RuntimeError("Contour goes outisde the bounds. "
-                                           "Set 'open_contours' to True if "
-                                           "that is the expected behavior.")
+                        raise RuntimeError(
+                            "Contour goes outisde the bounds. "
+                            "Set 'open_contours' to True if "
+                            "that is the expected behavior."
+                        ) from err
 
                 if ij == initial_point:
                     # The contour closes on itself
@@ -266,10 +308,17 @@ class MarchingSquares():
         return contours_cells, contour_paths
 
 
-def marching_cell_values(ij, d_ij, grid_values, x_array, y_array, level=0, mod=None):
+def marching_cell_values(
+    ij: PairInt,
+    d_ij: PairInt,
+    grid_values: npt.NDArray[np.float_],
+    x_array: npt.NDArray[np.float_],
+    y_array: npt.NDArray[np.float_],
+    level: float = 0.0,
+    mod: PairInt | None = None,
+) -> PairFloat:
     """
-    Return the interpolated values where the contour crosses
-    the new boundary between squares.
+    Return the interpolated values where the contour crosses the new boundary.
 
     Parameters
     ----------
@@ -281,14 +330,17 @@ def marching_cell_values(ij, d_ij, grid_values, x_array, y_array, level=0, mod=N
         Values to use in the linear interpolation.
     level : float, default to '0'
         The level of the isosurface, that is, contour in 2d.
-    mod: paif of ints
-        If
+    mod: paif of ints, optional
+        If provided, it indicates the size of the periodic grid.
+        Contours will wrap around the boundaries.
     """
     if mod is None:
-        mod = (2**32, 2**32)  # very large ints (no overflow in python 3)
+        _mod = (2**32, 2**32)
+    else:
+        _mod = mod
 
-    def values(i, j):
-        return grid_values[i % mod[0], j % mod[1]]
+    def values(i: int, j: int) -> float:
+        return grid_values[i % _mod[0], j % _mod[1]]
 
     i, j = ij
     if d_ij[0] == 1:
@@ -296,23 +348,23 @@ def marching_cell_values(ij, d_ij, grid_values, x_array, y_array, level=0, mod=N
         # | old | new |
         # o-----o-----x
         new_values = (
-            values(i+1, j),
-            values(i+1, j+1),
+            values(i + 1, j),
+            values(i + 1, j + 1),
         )
-        x = x_array[i+1]
+        x = x_array[i + 1]
         weight = (level - new_values[0]) / (new_values[1] - new_values[0])
-        y = y_array[j] + weight * (y_array[j+1] - y_array[j])
+        y = y_array[j] + weight * (y_array[j + 1] - y_array[j])
     elif d_ij[0] == -1:
         # x-----o-----o
         # | new | old |
         # x-----o-----o
         new_values = (
             values(i, j),
-            values(i, j+1),
+            values(i, j + 1),
         )
         x = x_array[i]
         weight = (level - new_values[0]) / (new_values[1] - new_values[0])
-        y = y_array[j] + weight * (y_array[j+1] - y_array[j])
+        y = y_array[j] + weight * (y_array[j + 1] - y_array[j])
     elif d_ij[1] == 1:
         # x-----x
         # | new |
@@ -320,12 +372,12 @@ def marching_cell_values(ij, d_ij, grid_values, x_array, y_array, level=0, mod=N
         # | old |
         # o-----o
         new_values = (
-            values(i, j+1),
-            values(i+1, j+1),
+            values(i, j + 1),
+            values(i + 1, j + 1),
         )
         weight = (level - new_values[0]) / (new_values[1] - new_values[0])
-        x = x_array[i] + weight * (x_array[i+1] - x_array[i])
-        y = y_array[j+1]
+        x = x_array[i] + weight * (x_array[i + 1] - x_array[i])
+        y = y_array[j + 1]
     elif d_ij[1] == -1:
         # o-----o
         # | old |
@@ -333,11 +385,11 @@ def marching_cell_values(ij, d_ij, grid_values, x_array, y_array, level=0, mod=N
         # | new |
         # x-----x
         new_values = (
-            values(i+1, j),
+            values(i + 1, j),
             values(i, j),
         )
         weight = (level - new_values[1]) / (new_values[0] - new_values[1])
-        x = x_array[i] + weight * (x_array[i+1] - x_array[i])
+        x = x_array[i] + weight * (x_array[i + 1] - x_array[i])
         y = y_array[j]
     else:
         raise ValueError(f"The displacement {d_ij} is trivial.")
@@ -345,8 +397,10 @@ def marching_cell_values(ij, d_ij, grid_values, x_array, y_array, level=0, mod=N
     return x, y
 
 
-def marching_step(cell, func, middle, d_ij):
-    """Return the direction to the next cell
+def marching_step(
+    cell: int, func: Callable[[float, float], float], middle: PairFloat, d_ij: PairInt
+) -> PairInt:
+    """Return the direction to the next cell.
 
 
     The parameters `func`, `middle` and `d_ij` are only accessed
@@ -361,51 +415,49 @@ def marching_step(cell, func, middle, d_ij):
         if cell == 0b0101:
             if func(*middle) < 0:
                 if d_ij == (0, 1):
-                    new_d_ij = tuple([1, 0])
+                    new_d_ij = (1, 0)
                 elif d_ij == (0, -1):
-                    new_d_ij = tuple([-1, 0])
+                    new_d_ij = (-1, 0)
                 raise RuntimeError(msg)
             else:
                 if d_ij == (0, 1):
-                    new_d_ij = tuple([-1, 0])
+                    new_d_ij = (-1, 0)
                 elif d_ij == (0, -1):
-                    new_d_ij = tuple([1, 0])
+                    new_d_ij = (1, 0)
                 raise RuntimeError(msg)
         elif cell == 0b1010:
             if func(*middle) < 0:
                 if d_ij == (1, 0):
-                    new_d_ij = tuple([0, -1])
+                    new_d_ij = (0, -1)
                 elif d_ij == (-1, 0):
-                    new_d_ij = tuple([0, 1])
+                    new_d_ij = (0, 1)
                 raise RuntimeError(msg)
             else:
                 if d_ij == (1, 0):
-                    new_d_ij = tuple([0, 1])
+                    new_d_ij = (0, 1)
                 elif d_ij == (-1, 0):
-                    new_d_ij = tuple([0, -1])
+                    new_d_ij = (0, -1)
                 raise RuntimeError(msg)
         else:
-            raise RuntimeError("cell " + bin(cell) + " shouldn't happen ...")
-
-        return new_d_ij
+            raise RuntimeError(f"cell {str(cell)} shouldn't happen ...")
 
 
 # tables definition
 """ MARCHING_STEPS : constant dict
     Assigns a direction to the square corner values.
 """
-MARCHING_STEPS = {
-    0b0000: tuple([1, 0]),
-    0b0001: tuple([-1, 0]),
-    0b0100: tuple([1, 0]),
-    0b0010: tuple([0, -1]),
-    0b1000: tuple([0, 1]),
-    0b0011: tuple([-1, 0]),
-    0b0110: tuple([0, -1]),
-    0b1100: tuple([1, 0]),
-    0b1001: tuple([0, 1]),
-    0b0111: tuple([-1, 0]),
-    0b1110: tuple([0, -1]),
-    0b1101: tuple([1, 0]),
-    0b1011: tuple([0, 1])
+MARCHING_STEPS: dict[int, PairInt] = {
+    0b0000: (1, 0),
+    0b0001: (-1, 0),
+    0b0100: (1, 0),
+    0b0010: (0, -1),
+    0b1000: (0, 1),
+    0b0011: (-1, 0),
+    0b0110: (0, -1),
+    0b1100: (1, 0),
+    0b1001: (0, 1),
+    0b0111: (-1, 0),
+    0b1110: (0, -1),
+    0b1101: (1, 0),
+    0b1011: (0, 1),
 }
